1c25f94995479f5380bf1e74a5eb80c070313b97
Bruno <brunolaltoe@gmail.com>
2023-07-03 10:29:15 -0300

    Fiz o pulo duplo e mais animações

--- Assets/Scripts/Personagem.cs
+++ Assets/Scripts/Personagem.cs
@@ -9,48 +9,111 @@ using Quaternion = UnityEngine.Quaternion;
 
 public class Personagem : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float jumpSpeed = 30.0f;
+    Rigidbody2D rb;
+    BoxCollider2D col;
     public Animator animator;
-    
-    private float directionLR;
-    private Vector2 horizontalMovement; // Talvez eu combine isso com directionLR
-    private float jumpInput;
-    private Rigidbody2D rb;
+
+    [SerializeField] LayerMask chaoPulavel;
+
+    #region Variáveis correr
+
+    public float moveSpeed;
+    public float jumpSpeed;
+    public float aceleracao;
+    public float desaceleracao;
+    [SerializeField] float velPower; // [SerializeField] == apareça no inspector, mesmo se for private
+    public float pulosExtras;
+
+    #endregion
+
+    bool pediuPular = false;
+    float moveInput = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        #region regulação de movimento
+
+        moveSpeed = 20.0f;
+        jumpSpeed = 23.0f;
+        aceleracao = 1.0f;
+        desaceleracao = 1.0f;
+        velPower = 2;
+        pulosExtras = 1;
+
+        #endregion
+
         rb= GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update é chamado continuamente
+    void Update () {
+        // pediuPular é false até o jogador apertar para pular.
+        if (pediuPular == false) 
+            pediuPular = Input.GetButtonDown("Jump");
+    }
+
+    // FixedUpdate é chamado toda vez que a engine de física rodar. Como não é todo frame, melhor receber os inputs muito curtos no Update.
+    void FixedUpdate()
     {
-        directionLR = Input.GetAxis("Horizontal");
 
-        // Vetor com movimento horizontal do personagem
-        horizontalMovement = Vector2.right * speed * directionLR * Time.deltaTime;
+        #region correr
+        moveInput = Input.GetAxis("Horizontal");
+
+        // Velocidade (e direção) que querermos alcançar
+        float targetSpeed = moveInput * moveSpeed;
+        // Pega a diferença entre a velocidade desejável e atual
+        float speedDiff = targetSpeed - rb.velocity.x;
+        // Aceleramos quando apertamos o botão. Desaceleramos quando não apertamos nada. Podemos querer que um aconteça mais rápido do que o outro, então separamos as situações.
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? aceleracao : desaceleracao;
+        // Multiplica a diferença de velocidade com a aceleração. Depois eleva isso a um número que quisermos. E finalmente, multiplicamos para voltar com o sinal.
+        float movimento = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velPower) * Mathf.Sign(speedDiff);
         
-        transform.Translate(horizontalMovement);
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMovement.x));        
+        rb.AddForce(movimento * Vector2.right);
 
-        if (directionLR > 0) {
+        #endregion
+        
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));  
+
+        ViraPersonagem();
+        
+        if (pediuPular) {
+            pediuPular = false;    
+            Pulo();
+        }
+
+    }
+
+    private bool IsGrounded() {
+        // Isso cria uma caixa do tamanho da hitbox, mas 0.1f abaixo dela. Checa se isso colide com algo que tenha o layer chaoPulavel, se sim retorna true.
+        return Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down, .1f, chaoPulavel);
+    }
+
+    private void Pulo () {
+        
+        // Não dá para pular, nesse caso.
+        if (!IsGrounded() && pulosExtras <= 0)
+            return;
+
+        if (!IsGrounded()) {
+            pulosExtras--;
+        }
+        else{
+            animator.SetBool("Jump", true);
+            pulosExtras = 1;
+        }
+
+        rb.AddForce(transform.up * jumpSpeed + new Vector3(0, -rb.velocity.y, 0), ForceMode2D.Impulse);
+    }
+
+    private void ViraPersonagem () {
+        if (moveInput > 0) {
             gameObject.transform.localScale = new Vector2(1,1);
         }
-        else if (directionLR < 0) {
+        else if (moveInput < 0) {
             gameObject.transform.localScale = new Vector2(-1,1);
         }
-
-        // Pulo
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(transform.up * jumpSpeed, ForceMode2D.Impulse);
-
-            // Faz ele parar de rodar
-            rb.angularVelocity = 0f;
-            // Alinha o personagem com o eixo y
-            transform.rotation = Quaternion.identity;
-        }
     }
+
 }
