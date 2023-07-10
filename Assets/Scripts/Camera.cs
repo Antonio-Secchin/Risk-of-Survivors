f7105ced451ddf446e2f41b147eaaad944f2e105
Bruno <brunolaltoe@gmail.com>
2023-07-09 23:29:53 -0300

    Animações de morte Necromante
    Agora ele mostra quando leva hit e quando morre.

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3 (0, 5, -10);
+    private Vector3 offset = new Vector3 (0, 2, -10);
     // Start is called before the first frame update
     void Start()
     {
